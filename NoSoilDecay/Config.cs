d71a5125399fe8b0be5d0f0342a3921f8ce4bbf2
A.Dude.Programmer <A.Dude.Programmer@gmail.com>
2021-02-14 12:12:22 -0700

    Initial Commit

---
+++ NoSoilDecay/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSoilDecayOnFarm
+{
+    public class Config
+    {
+        public bool farmonly { get; set; } = true;
+    }
+}
---
+++ NoSoilDecay/SaveData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Netcode;
+using StardewValley;
+using System.Collections.Generic;
+
+namespace NoSoilDecayOnFarm
+{
+    public class SaveData
+    {
+        public List<SaveTiles> data { get; set; } = new List<SaveTiles>();
+
+        public SaveData()
+        {
+        }
+
+        public SaveData(Dictionary<GameLocation, List<Vector2>> data)
+        {
+            this.data = new List<SaveTiles>();
+            using (Dictionary<GameLocation, List<Vector2>>.Enumerator enumerator = data.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    KeyValuePair<GameLocation, List<Vector2>> current = enumerator.Current;
+                    this.data.Add(new SaveTiles(current.Key.name, current.Value));
+                }
+            }
+        }
+    }
+}
